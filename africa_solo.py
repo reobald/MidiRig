dfbaf9d7429fe53a204620cc62da682a45941b38
Patrik Jonasson <patrik.jonasson@gmail.com>
2017-12-31 11:14:48 +0000

    adding licence info

--- africa_solo.py
+++ africa_solo.py
@@ -1,3 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2015-2017 Patrik Jonasson - All Rights Reserved
+#
+#
+# This file is part of MidiRig.
+#
+# MidiRig is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# MidiRig is distributed in the hope that it will be useful, but WITHOUT ANY     WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PART    ICULAR PURPOS$
+# See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along wi    th MidiRig.
+# If not, see <http://www.gnu.org licenses/>.
+
 from mididings.event import MidiEvent
 from mididings import NOTEON,NOTEOFF,Process
 
